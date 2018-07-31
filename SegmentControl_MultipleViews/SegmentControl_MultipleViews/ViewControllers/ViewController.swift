f10529f5e34dad6b7d454535f6fe7ed583783bd8
Rahul Patil <rahulpatil281292@gmail.com>
2018-07-31 14:18:35 +0530

    Update

---
+++ SegmentControl_MultipleViews/SegmentControl_MultipleViews/ViewControllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SegmentControl_MultipleViews
+//
+//  Created by ITC Infotech on 31/07/18.
+//  Copyright © 2018 Rahul Patil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
